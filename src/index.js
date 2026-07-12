106a637e70d7ffbe77064578373cd848e0fa2db5
Crazy4elovek <80841013+Crazy4elovek66@users.noreply.github.com>
2026-07-12 04:08:10 +0400

    Update index.js
    Signed-off-by: Crazy4elovek <80841013+Crazy4elovek66@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-import { connect } from 'cloudflare:sockets';
-import { handleRequest } from './handlers/main.js';
-
 export default {
-  async fetch(request, env, ctx) {
-    try {
-      return await handleRequest(request, env, ctx, connect);
-    } catch (e) {
-      return new Response(
-        JSON.stringify({
-          message: e?.message,
-          stack: e?.stack
-        }, null, 2),
-        {
-          status: 500,
-          headers: {
-            "content-type": "application/json"
-          }
-        }
-      );
-    }
+  async fetch() {
+    return new Response("OK");
   }
 }
